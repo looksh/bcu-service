454e6138001497be34880e2c4d737d75cfad5d1b
Looksh <lookshdev@gmail.com>
2022-10-22 22:03:08 +0900

    [update]포털 식단표 응답 받아오기
    원하는 날짜의 식단표를 요청하기 위해서는 sysDate 를 수정해야하는데 뒤에
    있는 값은 한글을 인코딩한 값임 (%EC 어쩌구)
    async 로 함수를 생성하고 그 안에 세 가지 요청 함수를 구현했음 ( 오늘,
    내일, 모레 ) (참고로 미완임)
    데이터 가공작업 및 요청 함수 알고리즘화 필요

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const axios = require("axios");
+const mealObj = [];
+const BCU_HEADER = {
+  headers: {
+    Accept: "application/json, text/javascript, */*; q=0.01",
+    "Accept-Language": "ko-KR,ko;q=0.6",
+    Connection: "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    Cookie:
+      "WMONID=P98Copi0smH; BCU_PT_SESSION=ODViZDYxMmMtOTRjMi00ZjY5LTk0MjgtNWI1ZmY3ZmY5ZWVk",
+    Origin: "https://portal.bc.ac.kr",
+    Referer: "https://portal.bc.ac.kr/main/indx/index.do",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-GPC": "1",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+    "X-CSRF-TOKEN": "3ca2b442-21a4-49da-a805-eed41bcceeb3",
+    "X-Requested-With": "XMLHttpRequest",
+  },
+};
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+async function main() {
+  // HTML 로드
+  const TodayResponse = await axios.post(
+    "https://portal.bc.ac.kr/pltl/food/findFoodData.json",
+    "sysDate=2022.10.24(%EC%9B%94)&toDay=20221024&weekDay=1&yyyy=2022&MM=10&dd=24",
+    BCU_HEADER
+  );
+
+  const TomorrowResponse = await axios.post(
+    "https://portal.bc.ac.kr/pltl/food/findFoodData.json",
+    "sysDate=2022.10.25(%ED%99%94)&toDay=20221025&weekDay=2&yyyy=2022&MM=10&dd=25",
+    BCU_HEADER
+  );
+
+  const mealList = JSON.stringify(TodayResponse.data.sosStudent1[0].bcMenuMenu);
+  const mealDay = JSON.stringify(TodayResponse.data.foodPortletParamVO.toDay);
+
+  const meal = JSON.stringify(TomorrowResponse.data.sosStudent1[0].bcMenuMenu);
+  const Day = JSON.stringify(TomorrowResponse.data.foodPortletParamVO.toDay);
+  console.log(mealDay);
+  console.log(mealList);
+  console.log(meal);
+  console.log(Day);
+}
+
+main();
