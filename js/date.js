1a43910e257fbfc19e9d43c3debe9009f713947d
Looksh <lookshdev@gmail.com>
2022-10-27 20:13:33 +0900

    [update] UTC+9 한국시간 적용 완료

--- js/date.js
+++ js/date.js
@@ -1,8 +1,9 @@
 const day = new Date();
+const KMTday = new Date(day.setHours(day.getHours() + 9));
 
 function dateCalc(param) {
-  day.setDate(day.getDate() + param);
-  let result = day.toISOString().substring(0, 10).replace(/-/g, "");
+  KMTday.setDate(KMTday.getDate() + param);
+  let result = KMTday.toISOString().substring(0, 10).replace(/-/g, "");
   return result;
 }
 
