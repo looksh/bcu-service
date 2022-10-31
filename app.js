4bfc865410826b4f729de41d76dc0946262bf1f1
Looksh <lookshdev@gmail.com>
2022-10-31 16:03:00 +0900

    [fix] 이거 이상하다.. post 를 함수로 요청하면 응답이 개느림 다시 원복

--- app.js
+++ app.js
@@ -9,32 +9,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api", apiRouter);
 
-function mealData(stu, stf, res) {
+apiRouter.post("/todaySosa", (req, res) => {
   const responseBody = {
     version: "2.0",
     data: {
       today: `${date.onel}`,
-      student: `${stu}`,
-      staff: `${stf}`,
+      student: `${process.todayStu}`,
+      staff: `${process.todayStf}`,
     },
   };
   res.status(200).send(responseBody);
-}
-
-apiRouter.post("/todaySosa", (req, res) => {
-  mealData(process.todayStu, process.todayStf, res);
 });
 
 apiRouter.post("/todayBcu", (req, res) => {
-  mealData(process.todayBtu, process.todayBtf, res);
+  const responseBody = {
+    version: "2.0",
+    data: {
+      today: `${date.onel}`,
+      student: `${process.todayBtu}`,
+      staff: `${process.todayBtf}`,
+    },
+  };
+  res.status(200).send(responseBody);
 });
 
 apiRouter.post("/tomorrowSosa", (req, res) => {
-  mealData(process.tomoStu, process.tomoStf, res);
+  const responseBody = {
+    version: "2.0",
+    data: {
+      today: `${date.neil}`,
+      student: `${process.tomoStu}`,
+      staff: `${process.tomoStf}`,
+    },
+  };
+  res.status(200).send(responseBody);
 });
 
 apiRouter.post("/tomorrowBcu", (req, res) => {
-  mealData(process.tomoBtu, process.tomoBtf, res);
+  const responseBody = {
+    version: "2.0",
+    data: {
+      today: `${date.neil}`,
+      student: `${process.tomoBtu}`,
+      staff: `${process.tomoBtf}`,
+    },
+  };
+  res.status(200).send(responseBody);
 });
 
 apiRouter.post("/notice", (req, res) => {
